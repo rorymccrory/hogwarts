d7490237b639af9efc0523f12025020b1cd709c4
Rory McCrory <rmccrory1998@gmail.com>
2022-04-10 15:30:18 -0500

    artifact assignment done! close #3

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/datainitializer/DBDataInitializer.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/datainitializer/DBDataInitializer.java
@@ -1,7 +1,9 @@
 package edu.tcu.cs.hogwartsartifactsonline.datainitializer;
 
 import edu.tcu.cs.hogwartsartifactsonline.dao.ArtifactDao;
+import edu.tcu.cs.hogwartsartifactsonline.dao.WizardDao;
 import edu.tcu.cs.hogwartsartifactsonline.domain.Artifact;
+import edu.tcu.cs.hogwartsartifactsonline.domain.Wizard;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,9 +11,11 @@ import org.springframework.stereotype.Component;
 public class DBDataInitializer implements CommandLineRunner {
 
     private ArtifactDao artifactDao;
+    private WizardDao wizardDao;
 
-    public DBDataInitializer(ArtifactDao artifactDao) {
+    public DBDataInitializer(ArtifactDao artifactDao, WizardDao wizardDao) {
         this.artifactDao = artifactDao;
+        this.wizardDao = wizardDao;
     }
 
     @Override
@@ -40,10 +44,37 @@ public class DBDataInitializer implements CommandLineRunner {
         a4.setDescription("the biggest wand ever");
         a4.setImageUrl("imageUrl");
 
-        artifactDao.save(a1);
-        artifactDao.save(a2);
-        artifactDao.save(a3);
-        artifactDao.save(a4);
+        Artifact a5 = new Artifact();
+        a5.setId("5250808601736515584");
+        a5.setName("Sword of Gryffindor");
+        a5.setDescription("A goblin made sword adorned with large rubies on the pommel");
+        a5.setImageUrl("imageUrl");
 
+        Artifact a6 = new Artifact();
+        a6.setId("6250808601736515584");
+        a6.setName("Resurrection Stone");
+        a6.setDescription("The Resurrection Stone allows the holder to bring back deceased");
+        a6.setImageUrl("imageUrl");
+
+
+        Wizard w1 = new Wizard();
+        w1.setName("Albus Dumbledore");
+        w1.addArtifact(a1);
+        w1.addArtifact(a3);
+
+        Wizard w2 = new Wizard();
+        w2.setName("Harry Potter");
+        w2.addArtifact(a2);
+        w2.addArtifact(a4);
+
+        Wizard w3 = new Wizard();
+        w3.setName("Neville Longbottom");
+        w3.addArtifact(a5);
+
+        wizardDao.save(w1);
+        wizardDao.save(w2);
+        wizardDao.save(w3);
+        //Since a6 doesn't have an owner yet, we need to save it explicitly here
+        artifactDao.save(a6);
     }
 }
